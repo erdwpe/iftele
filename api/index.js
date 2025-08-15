49961635567e638b66b48460c811a2de1966fdbe
erdwpe <90571955+erdwpe@users.noreply.github.com>
2025-08-15 17:31:08 +0700

    xixi

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'view', 'index.html');
-  const html = await fs.readFile(filePath, 'utf8');
+  try {
+    const filePath = path.join(process.cwd(), 'view', 'index.html');
+    const html = await fs.readFile(filePath, 'utf8');
 
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send(html);
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).end(html);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to load HTML' });
+  }
 }
