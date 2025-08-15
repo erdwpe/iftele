d91f3b5f9cd9c26ab83ea1e76663a9171dd9853c
erdwpe <90571955+erdwpe@users.noreply.github.com>
2025-08-15 17:32:41 +0700

    kwaokwaowa tolol

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -1,3 +1,4 @@
+export default async function handler(req, res) {
   const { key, text } = req.query;
 
   // Cek API key sederhana
@@ -26,3 +27,4 @@
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+}
