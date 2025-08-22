660a2899b39a29474d15c66e9a34fde66c1471a8
erdwpe <90571955+erdwpe@users.noreply.github.com>
2025-08-22 20:12:31 +0700

    kaokwaokwa

--- api/evening.js
+++ api/evening.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const url = "https://script.google.com/macros/s/AKfycbwSbJYOS08s0Bxwvt2v_HAF7l6rWpmYVX8BdpmCCKK0xV3rE8FWdjQQSXJKsHpsdnuaPg/exec?action=evening";
+  const url = "https://script.google.com/macros/s/AKfycbycSV_ONwUwV-Nm8akqwJmOZMsd7MEaVWiqt0gwkWCadrR9YrKqr4aC4SrfI6lmPI9dYw/exec?action=evening";
 
   try {
     const r = await fetch(url);
