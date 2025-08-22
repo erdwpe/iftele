bf76d4ecf4d8ff4655f638029acd958c66bbcca4
erdwpe <90571955+erdwpe@users.noreply.github.com>
2025-08-22 19:41:53 +0700

    hohoh

--- api/noon.js
+++ api/noon.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const url = "https://script.google.com/macros/s/AKfycbxXDzTjuIxp5M-zyS1UJHSuUYhQSXNyPcmXUBJh2v3jK4e9lDwp1_qAWekXPJz7NfYC0w/exec?action=noon";
+  const url = "https://script.google.com/macros/s/AKfycbwSbJYOS08s0Bxwvt2v_HAF7l6rWpmYVX8BdpmCCKK0xV3rE8FWdjQQSXJKsHpsdnuaPg/exec?action=noon";
 
   try {
     const r = await fetch(url);
