2eacff4fe5c27b995e067b729d48b1f600cd6daa
erdwpe <90571955+erdwpe@users.noreply.github.com>
2025-12-02 21:29:10 +0700

    dawdawdawda

--- api/evening.js
+++ api/evening.js
@@ -1,3 +1,5 @@
+//evening
+
 export default async function handler(req, res) {
   const url = "https://script.google.com/macros/s/AKfycbxWQj2buZii-Fj5l575dl_Vdobu4FQ8fIPf8B_FDyCNlEODTxTkJYdhsLFkROcuyOnxqQ/exec?action=evening";
 
